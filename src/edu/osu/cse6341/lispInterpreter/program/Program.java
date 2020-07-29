de290ee25e329cfedffd72241721cc148c37d397
Eric Soppi <soppi.2@osu.edu>
2020-07-29 11:00:56 -0700

    Remove startNode class

--- src/edu/osu/cse6341/lispInterpreter/program/Program.java
+++ src/edu/osu/cse6341/lispInterpreter/program/Program.java
@@ -1,32 +1,69 @@
 package edu.osu.cse6341.lispInterpreter.program;
 
-import edu.osu.cse6341.lispInterpreter.program.nodes.StartNode;
+import edu.osu.cse6341.lispInterpreter.constants.ReservedValuesConstants;
+import edu.osu.cse6341.lispInterpreter.program.nodes.LispNode;
+import edu.osu.cse6341.lispInterpreter.program.parser.Parser;
 import edu.osu.cse6341.lispInterpreter.tokenizer.Tokenizer;
+import edu.osu.cse6341.lispInterpreter.tokenizer.tokens.TokenKind;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program implements IPrettyPrintable{
 
-	private final StartNode rootNode;
+	private List<LispNode> rootNodes;
+	private List<LispNode> evaluatedNodes;
+	private boolean isEvaluated;
 
 	public Program(){
-        rootNode = new StartNode();
+        rootNodes = new ArrayList<>();
+        evaluatedNodes = new ArrayList<>();
+        isEvaluated = false;
 	}
 
 	public void parse(Tokenizer tokenizer) throws Exception{
-		rootNode.parse(tokenizer);
+		Parser parser = new Parser();
+		while (tokenizer.getCurrent().getTokenKind() != TokenKind.EOF_TOKEN) {
+			LispNode root = parser.parseIntoNode(tokenizer);
+			rootNodes.add(root);
+		}
 	}
 
 	public void evaluate() throws Exception{
-		rootNode.evaluate(true);
+		for(LispNode node: rootNodes) {
+			boolean isNotList = !node.isNodeList();
+			boolean isNotNumeric = !node.isNodeNumeric();
+			boolean isNotT = !node.getNodeValue().equals(ReservedValuesConstants.T);
+			boolean isNotNil = !node.getNodeValue().equals(ReservedValuesConstants.NIL);
+			if (isNotList && isNotNumeric && isNotT && isNotNil)
+				throw new Exception("Error! " + node.getNodeValue() + " is not a valid atomic value!\n");
+			LispNode evaluatedNode = ((IEvaluatable) node).evaluate(false);
+			if(evaluatedNode != null) evaluatedNodes.add(evaluatedNode);
+		}
+		isEvaluated = true;
 	}
 
 	@Override
 	public String getListNotationToString(boolean isFirst){
-	    return rootNode.getListNotationToString(isFirst);
+		List<LispNode> nodes = isEvaluated ? evaluatedNodes : rootNodes;
+		StringBuilder sb = new StringBuilder();
+		for (LispNode node : nodes) {
+			sb.append(((IPrettyPrintable)node).getListNotationToString(node.isNodeList()));
+			sb.append('\n');
+		}
+	    return sb.toString();
     }
 
     @Override
     public String getDotNotationToString(){
-	    return rootNode.getDotNotationToString();
-    }
+		List<LispNode> nodes = isEvaluated ? evaluatedNodes : rootNodes;
+		if (nodes.isEmpty()) return "NIL\n";
+		StringBuilder sb = new StringBuilder();
+		for (LispNode node : nodes) {
+			sb.append(((IPrettyPrintable)node).getDotNotationToString());
+			sb.append('\n');
+		}
+		return sb.toString();
+	}
 
 }
