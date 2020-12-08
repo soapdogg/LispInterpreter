c76b554489f68bcbca8ed87b7680b1d92d7987f4
Eric Soppi <eric.soppi@gmail.com>
2020-12-08 08:11:50 -0800

    Move nodeEvaluator recursive call to common place for all builtin functions

--- src/main/java/com/soapdogg/lispInterpreter/evaluator/NodeEvaluator.kt
+++ src/main/java/com/soapdogg/lispInterpreter/evaluator/NodeEvaluator.kt
@@ -99,182 +99,133 @@ class NodeEvaluator(
                             it,
                             expressionNode
                         )
+
+                        if (addressValue == FunctionNameConstants.QUOTE) {
+                            return@map expressionNode.children[1]
+                        }
+
+                        val evaluatedChildren = evaluateV2(
+                            expressionNode.children.subList(1, it),
+                            userDefinedFunctions,
+                            variableNameToValueMap
+                        )
+
+                        when (addressValue) {
+                            FunctionNameConstants.ATOM -> {
+                                val result = evaluatedChildren[0] !is ExpressionListNode
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.INT -> {
+                                if (evaluatedChildren[0] is ExpressionListNode) {
+                                    return@map nodeGenerator.generateAtomNode(false)
+                                } else {
+                                    val value = (evaluatedChildren[0] as AtomNode).value
+                                    val result = numericStringDeterminer.isStringNumeric(value)
+                                    return@map nodeGenerator.generateAtomNode(result)
+                                }
+                            }
+                            FunctionNameConstants.NULL -> {
+                                if (evaluatedChildren[0] is ExpressionListNode) {
+                                    return@map nodeGenerator.generateAtomNode(false)
+                                } else {
+                                    val value = (evaluatedChildren[0] as AtomNode).value
+                                    val result = nodeValueComparator.equalsNil(value)
+                                    return@map nodeGenerator.generateAtomNode(result)
+                                }
+                            }
+                            FunctionNameConstants.CAR -> {
+                                val evaluatedChildExpressionListNode = listValueRetriever.retrieveListValue(
+                                    evaluatedChildren[0],
+                                    FunctionNameConstants.CAR,
+                                    variableNameToValueMap
+                                )
+                                return@map evaluatedChildExpressionListNode.children[0]
+                            }
+                            FunctionNameConstants.CDR -> {
+                                val evaluatedChildExpressionList = listValueRetriever.retrieveListValue(
+                                    evaluatedChildren[0],
+                                    FunctionNameConstants.CDR,
+                                    variableNameToValueMap
+                                )
+                                when (evaluatedChildExpressionList.children.size) {
+                                    1 -> {
+                                        return@map evaluatedChildExpressionList.children[0]
+                                    }
+                                    2 -> {
+                                        return@map evaluatedChildExpressionList.children[1]
+                                    }
+                                    else -> {
+                                        return@map nodeGenerator.generateExpressionListNode(
+                                            evaluatedChildExpressionList.children.subList(1, evaluatedChildExpressionList.children.size)
+                                        )
+                                    }
+                                }
+                            }
+                            FunctionNameConstants.CONS -> {
+                                val evaluatedAddress = evaluatedChildren[0]
+                                val evaluatedData = evaluatedChildren[1]
+                                if (evaluatedData is ExpressionListNode) {
+                                    return@map nodeGenerator.generateExpressionListNode(
+                                        listOf(evaluatedAddress) + evaluatedData.children
+                                    )
+                                } else {
+                                    return@map nodeGenerator.generateExpressionListNode(
+                                        listOf(evaluatedAddress, evaluatedData)
+                                    )
+                                }
+                            }
+                            FunctionNameConstants.EQ -> {
+                                val leftValue = listNotationPrinter.printInListNotation(evaluatedChildren[0])
+                                val rightValue = listNotationPrinter.printInListNotation(evaluatedChildren[1])
+
+                                val result = leftValue == rightValue
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.GREATER -> {
+                                val numericChildren = numericValueRetriever.retrieveNumericValue(
+                                    evaluatedChildren,
+                                    FunctionNameConstants.GREATER
+                                )
+
+                                val result = numericChildren[0] > numericChildren[1]
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.LESS -> {
+                                val numericChildren = numericValueRetriever.retrieveNumericValue(
+                                    evaluatedChildren,
+                                    FunctionNameConstants.LESS
+                                )
+                                val result = numericChildren[0] < numericChildren[1]
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.MINUS -> {
+                                val numericChildren = numericValueRetriever.retrieveNumericValue(
+                                    evaluatedChildren,
+                                    FunctionNameConstants.MINUS
+                                )
+                                val result = numericChildren.reduce { acc, i -> acc - i }
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.PLUS -> {
+                                val numericChildren = numericValueRetriever.retrieveNumericValue(
+                                    evaluatedChildren,
+                                    FunctionNameConstants.PLUS
+                                )
+                                val result = numericChildren.reduce { acc, i -> acc + i }
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            FunctionNameConstants.TIMES -> {
+                                val numericChildren = numericValueRetriever.retrieveNumericValue(
+                                    evaluatedChildren,
+                                    FunctionNameConstants.TIMES
+                                )
+                                val result = numericChildren.reduce { acc, i -> acc * i }
+                                return@map nodeGenerator.generateAtomNode(result)
+                            }
+                            else -> return@let
+                        }
                     }
-
-                    when (addressValue) {
-                        FunctionNameConstants.ATOM -> {
-                            val evaluatedResult = evaluateV2(
-                                listOf(expressionNode.children[1]),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val result = evaluatedResult[0] !is ExpressionListNode
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.INT -> {
-                            val evaluatedResult = evaluateV2(
-                                listOf(expressionNode.children[1]),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            if (evaluatedResult[0] is ExpressionListNode) {
-                                nodeGenerator.generateAtomNode(false)
-                            } else {
-                                val value = (evaluatedResult[0] as AtomNode).value
-                                val result = numericStringDeterminer.isStringNumeric(value)
-                                nodeGenerator.generateAtomNode(result)
-                            }
-                        }
-                        FunctionNameConstants.NULL -> {
-                            val evaluatedResult = evaluateV2(
-                                listOf(expressionNode.children[1]),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            if (evaluatedResult[0] is ExpressionListNode) {
-                                nodeGenerator.generateAtomNode(false)
-                            } else {
-                                val value = (evaluatedResult[0] as AtomNode).value
-                                val result = nodeValueComparator.equalsNil(value)
-                                nodeGenerator.generateAtomNode(result)
-                            }
-                        }
-                        FunctionNameConstants.QUOTE -> {
-                            expressionNode.children[1]
-                        }
-                        FunctionNameConstants.CAR -> {
-                            val evaluatedChild = evaluateV2(
-                                listOf(expressionNode.children[1]),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val evaluatedChildExpressionListNode = listValueRetriever.retrieveListValue(
-                                evaluatedChild[0],
-                                FunctionNameConstants.CAR,
-                                variableNameToValueMap
-                            )
-                            evaluatedChildExpressionListNode.children[0]
-                        }
-                        FunctionNameConstants.CDR -> {
-                            val evaluatedChild = evaluateV2(
-                                listOf(expressionNode.children[1]),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val evaluatedChildExpressionList = listValueRetriever.retrieveListValue(
-                                evaluatedChild[0],
-                                FunctionNameConstants.CDR,
-                                variableNameToValueMap
-                            )
-                            if (evaluatedChildExpressionList.children.size == 1) {
-                                evaluatedChildExpressionList.children[0]
-                            } else if (evaluatedChildExpressionList.children.size == 2) {
-                                evaluatedChildExpressionList.children[1]
-                            } else {
-                                nodeGenerator.generateExpressionListNode(
-                                    evaluatedChildExpressionList.children.subList(1, evaluatedChildExpressionList.children.size)
-                                )
-                            }
-                        }
-                        FunctionNameConstants.CONS -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val evaluatedAddress = evaluatedChildren[0]
-                            val evaluatedData = evaluatedChildren[1]
-                            if (evaluatedData is ExpressionListNode) {
-                                nodeGenerator.generateExpressionListNode(
-                                    listOf(evaluatedAddress) + evaluatedData.children
-                                )
-                            } else {
-                                nodeGenerator.generateExpressionListNode(
-                                    listOf(evaluatedAddress, evaluatedData)
-                                )
-                            }
-                        }
-                        FunctionNameConstants.EQ -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val leftValue = listNotationPrinter.printInListNotation(evaluatedChildren[0])
-                            val rightValue = listNotationPrinter.printInListNotation(evaluatedChildren[1])
-
-                            val result = leftValue == rightValue
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.GREATER -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val numericChildren = numericValueRetriever.retrieveNumericValue(
-                                evaluatedChildren,
-                                FunctionNameConstants.GREATER
-                            )
-
-                            val result = numericChildren[0] > numericChildren[1]
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.LESS -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val numericChildren = numericValueRetriever.retrieveNumericValue(
-                                evaluatedChildren,
-                                FunctionNameConstants.LESS
-                            )
-                            val result = numericChildren[0] < numericChildren[1]
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.MINUS -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val numericChildren = numericValueRetriever.retrieveNumericValue(
-                                evaluatedChildren,
-                                FunctionNameConstants.MINUS
-                            )
-                            val result = numericChildren.reduce { acc, i -> acc - i }
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.PLUS -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val numericChildren = numericValueRetriever.retrieveNumericValue(
-                                evaluatedChildren,
-                                FunctionNameConstants.PLUS
-                            )
-                            val result = numericChildren.reduce { acc, i -> acc + i }
-                            nodeGenerator.generateAtomNode(result)
-                        }
-                        FunctionNameConstants.TIMES -> {
-                            val evaluatedChildren = evaluateV2(
-                                expressionNode.children.subList(1, 3),
-                                userDefinedFunctions,
-                                variableNameToValueMap
-                            )
-                            val numericChildren = numericValueRetriever.retrieveNumericValue(
-                                evaluatedChildren,
-                                FunctionNameConstants.TIMES
-                            )
-                            val result = numericChildren.reduce { acc, i -> acc * i }
-                            nodeGenerator.generateAtomNode(result)
-                        }
-
-                        else -> throw Exception("Error! Invalid CAR value: $addressValue\n")
-                    }
+                    throw Exception("Error! Invalid CAR value: $addressValue\n")
                 }
             }
         }
